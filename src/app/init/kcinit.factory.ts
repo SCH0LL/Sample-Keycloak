4709e1e6649ce974db15fc7d50f427485c774eb9
SCH0LL <michi.scholl@live.de>
2022-04-14 18:11:32 +0200

    Added Code from Tutorial

---
+++ src/app/init/kcinit.factory.ts
@@ -0,0 +1,7 @@
+import { KeycloakService } from "keycloak-angular";
+
+export function initializeKeycloak(keycloak: KeycloakService) {
+    return() => keycloak.init({
+        config: {url: 'http://localhost:8080' +'/auth', realm: 'main', clientId: 'Keycloak-Sample'}
+    });
+}
